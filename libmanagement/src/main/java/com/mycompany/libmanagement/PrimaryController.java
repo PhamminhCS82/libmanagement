59f6d31ec37ab47e266359df67039ab4231a95d4
PhamminhCS82 <pminh723@gmail.com>
2020-11-21 01:31:14 +0700

    xu ly danh sach (chua xong)

--- libmanagement/src/main/java/com/mycompany/libmanagement/PrimaryController.java
+++ libmanagement/src/main/java/com/mycompany/libmanagement/PrimaryController.java
@@ -1,12 +1,33 @@
 package com.mycompany.libmanagement;
 
-import java.io.IOException;
+import com.pqm.pojo.Books;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class PrimaryController {
-
+    
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TableView<Books> tbBooks;
+    public void initialize(URL url, ResourceBundle rb){
+        this.loadBooks();
+    }
+    private void loadBooks(){
+        TableColumn clName = new TableColumn("Tên sách");
+        TableColumn clPublisher = new TableColumn("Nhà xuất bản");
+        TableColumn clAuthor = new TableColumn("Tác giả");
+        TableColumn clCategory = new TableColumn("Thể loại");
+        TableColumn clYear = new TableColumn("Năm xuất bản");
+        TableColumn clDescribe = new TableColumn("Thông tin");
+        this.tbBooks.getColumns().addAll(clName);
+        this.tbBooks.getColumns().addAll(clPublisher);
+        this.tbBooks.getColumns().addAll(clAuthor);
+        this.tbBooks.getColumns().addAll(clCategory);
+        this.tbBooks.getColumns().addAll(clYear);
+        this.tbBooks.getColumns().addAll(clDescribe);
+        //this.tbBooks.setItems(FXCollections.observableArrayList(""));
     }
 }
--- libmanagement/src/main/java/com/pqm/pojo/Books.java
+++ libmanagement/src/main/java/com/pqm/pojo/Books.java
@@ -10,9 +10,118 @@ package com.pqm.pojo;
  * @author pminh
  */
 public class Books {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the describe
+     */
+    public String getDescribe() {
+        return describe;
+    }
+
+    /**
+     * @param describe the describe to set
+     */
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    /**
+     * @return the publisher
+     */
+    public String getPublisher() {
+        return publisher;
+    }
+
+    /**
+     * @param publisher the publisher to set
+     */
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    /**
+     * @return the year
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
     private int id;
     private String name;
     private String author;
     private String describe;
     private String publisher;
+    private int year;
+    private String category;
+    public Books(String name, String author,
+            String describe, String publisher, String category, int year){
+        this.name = name;
+        this.author = author;
+        this.describe = describe;
+        this.publisher = publisher;
+        this.category = category;
+        this.year = year;
+    }
 }
