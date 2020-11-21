01e9dc0356214041e0dd0f5df7b2c97ac4de9c1a
tatquangkiet789 <52742909+tatquangkiet789@users.noreply.github.com>
2020-11-21 17:14:07 +0700

    Thêm file userform.fxml, thêm folder styles chứa file userform.css, sửa App.java hàm start chỗ loadFXML() từ primary sang userform

--- libmanagement/src/main/java/module-info.java
+++ libmanagement/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.libmanagement {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.base;
 
     opens com.mycompany.libmanagement to javafx.fxml;
     exports com.mycompany.libmanagement;
