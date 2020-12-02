92a0dbcdac220b15fb312fd0a9f7badf53da8586
tatquangkiet789 <52742909+tatquangkiet789@users.noreply.github.com>
2020-12-02 18:16:05 +0700

    Them trang register.fxml va register.css

--- libmanagement/src/main/java/com/mycompany/libmanagement/App.java
+++ libmanagement/src/main/java/com/mycompany/libmanagement/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 300, 450);
+        scene = new Scene(loadFXML("register"), 750, 550);
         stage.setScene(scene);
         stage.show();
     }
